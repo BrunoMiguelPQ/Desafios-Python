d71303364f2b432ef84e4afd4b86fe9039ebf2b6
Bruno Queirós <brunompq8@gmail.com>
2025-07-16 11:28:09 +0100

    Contagem Regressiva

---
+++ Contagem Regressiva.py
@@ -0,0 +1,8 @@
+#Contagem regressiva 0 para 10
+from time import sleep
+print('Será dada a partida em: ')
+sleep(2)
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print('\033[32mP A R T I D A A A A A A A !!!!!!')
