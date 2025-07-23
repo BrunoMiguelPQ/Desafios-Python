94a619acd7e3042627c8c2f27c18c7708dfc7134
Bruno Queirós <brunompq8@gmail.com>
2025-07-23 18:34:37 +0100

    Analisador de Triângulos 2.0

---
+++ ex042.py
@@ -0,0 +1,19 @@
+#CONDIÇÃO Aninhada
+#Analisando Triangulos
+print('-='*15)
+print('Analisador de triangulos')
+print('-='*15)
+n1 = float(input('Reta 1: '))
+n2 = float(input('Reta 2: '))
+n3 = float(input('Reta 3: '))
+if n1 < n2 + n3 and n2 < n1 + n3 and n3 < n1 + n2:
+    print('Podem formar um triangulo', end=' ')
+    if n1 == n2 == n3:
+        print('EQUILATERO')
+    elif n1 != n2 and n1 != n3 and n2 != n3:
+        print('ESCALENO')
+    else:
+        print('ISÓSCELES')
+
+else:
+    print('Não podem formar um triangulo')
